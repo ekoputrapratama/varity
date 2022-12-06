ae1eb5fe7acb3075692252538245e6c5e18516fe
muhammadsayuti <muhammad.sayuti94@gmail.com>
2022-12-07 07:39:14 +0800

    change .toDouble to .0 for shorter syntax

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,6 @@
 // it have a max brightness up to 2047 so I'll put this here for now, in
 // hope that the android team can add this feature
 final Map<String, double> maxBrightnessMap = {
-  "Xiaomi M2101K6G": 2047.toDouble(),
-  "default": 255.toDouble()
+  "Xiaomi M2101K6G": 2047.0,
+  "default": 255.0
 };
